3c243fa4bf1bb08396561cec75b7d4e08828cf11
aidanbell <aidanbell0182@gmail.com>
2020-05-26 13:57:25 -0400

    Live preview working. Scrollsync between input/output working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import { ScrollSync, ScrollSyncPane } from 'react-scroll-sync';
+
 import './App.css';
 import '98.css';
 
+var hljs = require('highlight.js')
 var newLine = require('markdown-it-preserve-newline');
-var md = require('markdown-it')().use(newLine)
+var md = require('markdown-it')({
+  typographer: true,
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return '<pre class="hljs"><code>' +
+               hljs.highlight(lang, str, true).value +
+               '</code></pre>';
+      } catch (__) {}
+    }
+
+    return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+  }
+}).use(newLine)
 
 class App extends Component {
   constructor() {
@@ -14,13 +30,14 @@ class App extends Component {
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleReset = this.handleReset.bind(this)
   }
 
   handleChange(e) {
-    let r = md.render(this.state.input)
+    let r = md.render(e.target.value)
     this.setState({
       [e.target.name]: e.target.value,
-      result: md.render(this.state.input)
+      result: r
     })
   }
 
@@ -32,20 +49,39 @@ class App extends Component {
     })
   }
 
+  handleReset() {
+    this.setState({
+      input: '',
+      result: ''
+    })
+  }
+
 render() {
   return (
-    <div className="App">
-      <h1>Welcome To Markdown98</h1>
-      <container>
-        <div className="input">
-          <form name="input" onSubmit={this.handleSubmit}>
-            <textarea name="input" id="editor" value={this.state.input} onChange={this.handleChange} autoFocus></textarea>
-            <button type="submit"></button>
-          </form>
-        </div>
-        <div className="output" dangerouslySetInnerHTML={{__html: this.state.result}}></div>
-      </container>
-    </div>
+    <ScrollSync>
+      <div className="App">
+        <h1>Welcome To Markdown98</h1>
+        <container>
+          <div className="input">
+            <div className="title-bar">
+              <p className="title-bar-text">Write your boring, plain-text here</p>
+              <button aria-label="Close" onClick={this.handleReset}></button>
+            </div>
+            <ScrollSyncPane>
+              <textarea name="input" id="editor" value={this.state.input} onChange={this.handleChange} autoFocus></textarea>
+            </ScrollSyncPane>
+          </div>
+          <div className="output">
+            <div className="title-bar">
+              <p className="title-bar-text">Get your sweet, sweet, Markdown here:</p>
+            </div>
+            <ScrollSyncPane>
+              <div className="result" dangerouslySetInnerHTML={{__html: this.state.result}}></div>
+            </ScrollSyncPane>
+          </div>
+        </container>
+      </div>
+    </ScrollSync>
   )};
 }
 
