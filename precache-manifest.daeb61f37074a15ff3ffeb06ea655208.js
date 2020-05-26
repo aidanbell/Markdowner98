00b83d4c3bfe2330c8ac8a2e7daf9a2d268ccc30
aidanbell <aidanbell0182@gmail.com>
2020-05-26 15:13:12 -0400

    Updates

---
+++ precache-manifest.daeb61f37074a15ff3ffeb06ea655208.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db47e3e4d98ef6493d353b8cca5b30ee",
+    "url": "/markdowner98/index.html"
+  },
+  {
+    "revision": "cff84d5f6fdd02dc89d9",
+    "url": "/markdowner98/static/css/2.e0523291.chunk.css"
+  },
+  {
+    "revision": "0edd23888df0c1af6199",
+    "url": "/markdowner98/static/css/main.4f7a69de.chunk.css"
+  },
+  {
+    "revision": "cff84d5f6fdd02dc89d9",
+    "url": "/markdowner98/static/js/2.47b9e1e5.chunk.js"
+  },
+  {
+    "revision": "7a29fd67914f99c562dc3f65bbf1078b",
+    "url": "/markdowner98/static/js/2.47b9e1e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0edd23888df0c1af6199",
+    "url": "/markdowner98/static/js/main.5c7fe8d7.chunk.js"
+  },
+  {
+    "revision": "2908f427f75019a115d3",
+    "url": "/markdowner98/static/js/runtime-main.615054cc.js"
+  },
+  {
+    "revision": "1d4d404a3bbe4b7007f89e792233aa9c",
+    "url": "/markdowner98/static/media/ms_sans_serif.1d4d404a.woff2"
+  },
+  {
+    "revision": "7bbbd32e0f9977dd70a4d1287927d5da",
+    "url": "/markdowner98/static/media/ms_sans_serif.7bbbd32e.woff"
+  },
+  {
+    "revision": "6eb2c63fbead0014ef0c4c537bfe328a",
+    "url": "/markdowner98/static/media/ms_sans_serif_bold.6eb2c63f.woff"
+  },
+  {
+    "revision": "cb1c7473de82bb53662aaa2d7640e53e",
+    "url": "/markdowner98/static/media/ms_sans_serif_bold.cb1c7473.woff2"
+  }
+]);
